0ad255c8bfa6149425754af67b7d97a1729db2c8
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-03-21 10:14:11 -0400

    update bot

--- Challenge_1/bot/bot.py
+++ Challenge_1/bot/bot.py
@@ -7,15 +7,14 @@ options.add_argument('--headless')
 driver = webdriver.Chrome('./chromedriver',options=options)
 
 print("First failed ping")
-
-driver.get("http://127.0.0.1:3000/")
-driver.add_cookie({"name" : "AUTH_COOKIE", "value" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFhYSIsImlhdCI6MTY3ODc2OTYyNCwiZXhwIjoxNjc4ODU2MDI0fQ.GMJeLwxcROVx-0YTjsaQg3mP7L_mUuCam-PCeGU9z-o", "domain": "127.0.0.1"})
+driver.get("http://54.224.199.209")
+driver.add_cookie({"name" : "AUTH_COOKIE", "value" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFkbWluIiwiaWF0IjoxNjc5MTUyMjQzLCJleHAiOjE2NzkyMzg2NDN9.CheD3GBRTIP71_BvijVPgJT35bxJI6WwH8WRN13Vku8", "domain": "54.224.199.209"})
 
 print("Start spam")
 
 while True :
     print("Ping server")
-    driver.get("http://127.0.0.1:3000/")
+    driver.get("http://54.224.199.209")
     time.sleep(15)
 
 driver.close()
